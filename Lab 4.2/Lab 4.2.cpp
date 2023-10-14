2e0ebbb7940b452abe784d21da47e5736f0b2924
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-10-14 17:29:23 +0300

    La

---
+++ Lab 4.2/Lab 4.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, start, end, step, a, b, y;
+	cout << "xp = "; cin >> start;
+	cout << "xk = "; cin >> end;
+	cout << "dx = "; cin >> step;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = start;
+	while (x <= end)
+	{
+		a = ((2 + x) / pow(x, 2)) + 1;
+		if (x < 0)
+			b = pow(x, 3) - 2 * pow(x, 4);
+		else
+			if (x >= 0 && x <= 2)
+				b = pow(abs(x) + exp(x), 3);
+			else
+				b = 4 * cos(pow(x, 2) - 2);
+		y = a + b;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += step;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
